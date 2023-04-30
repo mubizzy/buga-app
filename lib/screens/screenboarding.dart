da5f47fdfde00ef361dcb68c3ec16ef48bd0994c
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-04-30 09:54:14 -0800

    screen

---
+++ lib/screens/screenboarding.dart
@@ -0,0 +1,12 @@
+import 'package:buga_app/splash/component/body.dart';
+
+import 'package:flutter/material.dart';
+
+class ScreenBoarding extends StatelessWidget {
+  const ScreenBoarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Body();
+  }
+}
--- lib/screens/sign_up_page.dart
+++ lib/screens/sign_up_page.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import '../config.dart';
 import '../constant.dart';
 import '../splash/component/widget/exports.dart';
-// import 'body_signup.dart';
+import '../validation.dart';
+import 'login_page.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -41,60 +42,139 @@ class _SignUpPageState extends State<SignUpPage> {
     double height = SizeConfig.blockSizeV!;
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Column(
-                children: [
-                  Text(
-                    'Sign Up',
-                    style: kTitle2,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: height * 2,
-              ),
-              Text("Create an account to get started...", style: kBodyText1),
-              SizedBox(
-                height: height * 4,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
                   children: [
-                    SizedBox(
-                      height: height * 4,
+                    Text(
+                      'Sign Up',
+                      style: kTitle2,
                     ),
-                    MyTextFormField(
-                      fillColor: Colors.white,
-                      hint: 'Name',
-                      icon: Icons.person,
-                      inputAction: TextInputAction.next,
-                      inputType: TextInputType.name,
-                      focusNode: _signUpFocusNodes[0],
-                    ),
-                    Text("Email"),
-                    MyTextFormField(
-                      hint: 'Email',
-                      icon: Icons.email_outlined,
-                      fillColor: Colors.white,
-                      inputType: TextInputType.emailAddress,
-                      inputAction: TextInputAction.next,
-                      focusNode: _signUpFocusNodes[1],
-                    ),
-                    MyPasswordField(
-                        fillColor: Colors.white,
-                        focusNode: _signUpFocusNodes[2]),
-                    MyCheckBox(
-                        text: "I agree to the terms of service and privacy")
                   ],
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: height * 2,
+                ),
+                Text("Create an account to get started...", style: kBodyText1),
+                SizedBox(
+                  height: height * 4,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Form(
+                    key: _signUpKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: height * 4,
+                        ),
+                        Text("Full Name", style: kBodyText3),
+                        MyTextFormField(
+                          fillColor: Colors.white,
+                          hint: 'Name',
+                          icon: Icons.person,
+                          inputAction: TextInputAction.next,
+                          inputType: TextInputType.name,
+                          focusNode: _signUpFocusNodes[0],
+                          validator: nameValidator,
+                        ),
+                        Text("Email", style: kBodyText3),
+                        MyTextFormField(
+                          hint: 'Email',
+                          icon: Icons.email_outlined,
+                          fillColor: Colors.white,
+                          inputType: TextInputType.emailAddress,
+                          inputAction: TextInputAction.next,
+                          focusNode: _signUpFocusNodes[1],
+                          validator: emailValidator,
+                        ),
+                        Text("Password", style: kBodyText3),
+                        MyPasswordField(
+                          fillColor: Colors.white,
+                          focusNode: _signUpFocusNodes[2],
+                          validator: passwordValidator,
+                        ),
+                        const MyCheckBox(
+                            text:
+                                "I agree to the terms of service and privacy"),
+                        SizedBox(
+                          height: height * 4,
+                        ),
+                        NewButton(
+                            buttonName: "Sign Up",
+                            onPressed: onSubmit,
+                            bgColor: const Color(0xff003049))
+                      ],
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Already have an account? ',
+                      style: kBodyText3,
+                    ),
+                    const SmallTextButton(
+                      buttonText: "Log In",
+                      page: LoginPage(),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: height * 2,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          height: 3,
+                          color: kSecondaryColor.withOpacity(0.4),
+                        ),
+                      ),
+                      Text(
+                        'Or sign in with',
+                        style: kBodyText3,
+                      ),
+                      Expanded(
+                        child: Divider(
+                          height: 3,
+                          color: kSecondaryColor.withOpacity(0.4),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: height * 2,
+                ),
+                Row(
+                  children: const [
+                    Expanded(
+                      child: LargeIconButton(
+                        buttonName: 'Google',
+                        iconImage: 'assets/images/facebook.png',
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Expanded(
+                        child: LargeIconButton(
+                      buttonName: 'Facebook',
+                      iconImage: 'assets/images/google.png',
+                    )),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
