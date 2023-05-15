6a027fb07649c2b7279d687a03b21fec72747e94
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-05-14 22:14:17 -0800

    sign up

--- lib/screens/sign_up_page.dart
+++ lib/screens/sign_up_page.dart
@@ -193,7 +193,3 @@ class _SignUpPageState extends State<SignUpPage> {
     );
   }
 }
-// Navigator.push(
-                              //     context,
-                              //     MaterialPageRoute(
-                              //         builder: (context) => ProfilePage()));
